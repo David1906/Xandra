0642bda2f36f3d230e18901996a624c453d977a6
David Ascencio <david1906@outlook.com>
2023-02-16 11:05:04 -0600

    Feat: Main config cached

--- DataAccess/MainConfigData.py
+++ DataAccess/MainConfigData.py
@@ -1,18 +1,31 @@
 import json
+import os
 
 
 class MainConfigData:
     MAIN_CONFIG_JSON_PATH = "xandra_config.json"
     YIELD_WARNING_MAX = 99
     YIELD_WARNING_THRESHOLD_FROM_ERROR = 10
+    JSON_DATA = {}
+    JSON_LAST_MODIFIED = None
+
+    def _getJsonData(self):
+        modifiedDate = os.path.getctime(MainConfigData.MAIN_CONFIG_JSON_PATH)
+        if (
+            MainConfigData.JSON_LAST_MODIFIED == None
+            or modifiedDate > MainConfigData.JSON_LAST_MODIFIED
+        ):
+            with open(MainConfigData.MAIN_CONFIG_JSON_PATH) as json_file:
+                MainConfigData.JSON_DATA = json.load(json_file)
+                MainConfigData.JSON_LAST_MODIFIED = modifiedDate
+        return MainConfigData.JSON_DATA
 
     def _getValue(self, key: str):
-        with open(MainConfigData.MAIN_CONFIG_JSON_PATH) as json_file:
-            data = json.load(json_file)
-            if type(data) is dict:
-                value = data[key]
-                if value != None:
-                    return value
+        data = self._getJsonData()
+        if type(data) is dict:
+            value = data[key]
+            if value != None:
+                return value
         return ""
 
     def get_disabled_fixture_fullpath(self) -> str:

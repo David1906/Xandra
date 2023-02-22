85d1480d6372be64732ca78add6b71866016a754
David Ascencio <david1906@outlook.com>
2023-02-22 15:40:28 -0600

    Fix: Wait 2 seconds in yield validation

--- Resources/chk_station_yield.sh
+++ Resources/chk_station_yield.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+RESULTFILE="/usr/local/Foxconn/automation/Xandra/Resources/chk_station_yield.result"
 python3 /usr/local/Foxconn/automation/Xandra/Resources/chk_station_yield.py
+read -t 2 -p "Validating yield..."

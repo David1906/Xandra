f5a7e87a3dab1dd4ffd299e25f1db04e2de26e36
David Ascencio <david1906@outlook.com>
2023-02-27 11:07:59 -0600

    Fix: Open logfile with space in name

--- Views/LogButton.py
+++ Views/LogButton.py
@@ -6,8 +6,10 @@ class LogButton(QtWidgets.QPushButton):
     def __init__(self, fullPath: str, text: str = "Open") -> None:
         super().__init__(text)
 
-        self._fullPath = fullPath
+        self._fullPath = (
+            fullPath.replace(" ", "\ ").replace("(", "\(").replace(")", "\)")
+        )
         self.clicked.connect(self.on_click)
 
     def on_click(self):
-        subprocess.Popen(["gedit", self._fullPath])
+        subprocess.Popen(["gedit " + self._fullPath], shell=True)

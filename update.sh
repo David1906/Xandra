68ab917ce01218c4b8d7e9db8f8a4b481cfed265
David Ascencio <david1906@outlook.com>
2023-03-06 10:25:56 -0600

    Core: Update file path change

--- update.sh
+++ update.sh
@@ -1,16 +1,15 @@
 cd "$(dirname "$0")"
 
-git restore .
+git checkout -f
 git clean -f
 git pull
 
-sudo chmod +x ./Resources/chk_station_is_disabled.py
-sudo chmod +x ./Resources/chk_station_is_disabled.sh
-sudo chmod +x ./Resources/chk_station_test_finished.py
-sudo chmod +x ./Resources/test_upload.sh
+sudo chmod +x Resources/chk_station_is_disabled.py
+sudo chmod +x Resources/chk_station_test_finished.py
+sudo chmod +x Resources/test_upload.sh
 
 if command -v alembic > /dev/null; then
-    echo "alembic upgrade head"
+    alembic upgrade head
 else
-    echo "/root/.local/bin/alembic upgrade head"
+    /root/.local/bin/alembic upgrade head
 fi

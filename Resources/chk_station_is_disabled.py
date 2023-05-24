2145d91112c56d1722437439f5ec87d11ca4a66f
David Ascencio <david1906@outlook.com>
2023-05-24 15:23:28 -0600

    Fix: Notify finish when fixture is disabled

--- Resources/chk_station_is_disabled.py
+++ Resources/chk_station_is_disabled.py
@@ -2,10 +2,9 @@
 from FixtureSocketNotifier import FixtureSocketNotifier
 import os
 
-
-if FixtureSocketNotifier(resultFileName="chk_station_yield.result").is_locked(
-    fixtureIp=os.getenv("XANDRA_FIXTURE_IP")
-):
+fixtureSocketNotifier = FixtureSocketNotifier(resultFileName="chk_station_yield.result")
+if fixtureSocketNotifier.is_locked(fixtureIp=os.getenv("XANDRA_FIXTURE_IP")):
     exit(0)
 else:
+    fixtureSocketNotifier.notify_finish(fixtureIp=os.getenv("XANDRA_FIXTURE_IP"))
     exit(1)

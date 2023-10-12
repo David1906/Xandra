a83135068f50419b89af0b484db8d58b2b8e4819
David Ascencio <david1906@outlook.com>
2023-10-12 13:53:55 -0600

    Core: Update version

--- xandra.py
+++ xandra.py
@@ -12,7 +12,7 @@ app = QApplication(sys.argv)
 
 
 def start():
-    os.environ["XANDRA_VERSION"] = "2.0.3 100423"
+    os.environ["XANDRA_VERSION"] = "3.0.0 101223"
     os.environ["HALT"] = "Az3E4ur"
     QtExceptHook().enable()
     with open(PathHelper().get_root_path() + "/Static/styles.css", "r") as fh:

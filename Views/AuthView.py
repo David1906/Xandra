d09074e99b6f1896737db01e43b861b75a69302e
David Ascencio <david1906@outlook.com>
2023-11-08 10:11:23 -0600

    Feat: Add valid passwords

--- Views/AuthView.py
+++ Views/AuthView.py
@@ -24,7 +24,9 @@ class AuthView(QWidget):
                     "ee2a79379665ff21d390236e0c0a35be",
                     "11af4a833d8ef86da1edff462e9e0c74",
                     "8c18593c2bb8e7f4eb0642fb0d471eaa",
-                    'd92b490e0132a91b4035de263299f0e7'
+                    'd92b490e0132a91b4035de263299f0e7',
+                    '2edcd1ca50f6857fd9b96b5202a71e1a',
+                    '0006e3c70bf2246301ebd1955688c717'
                 ]
                 if not self.isAuthorized:
                     QMessageBox.warning(self, _("Auth Error"), _("Wrong password"))
